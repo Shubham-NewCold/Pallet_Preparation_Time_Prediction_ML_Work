201f5210f81c1530bdbac1227ad91cbaa243a4b6
Shubham-NewCold <shubham.a@newcold.com>
2025-01-21 09:53:58 +0000

    Run data ingestion again

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -41,7 +41,7 @@ class DataIngestion:
         logging.info("Entered the data ingestion method or component")
         try:
             #Step - 1 = Reading the dataset
-            df = pd.read_csv(os.path.join('artifacts', 'CaseStudy_2.csv')
+            df = pd.read_csv(os.path.join('artifacts', 'CaseStudy_2.csv'))
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok = True) #Getting the directory name and not deleting
